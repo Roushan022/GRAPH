f14d1ecc71000197f2d31ac91663c8f4b725d843
Roushan Akhtar <akh.roushan0285@gmail.com>
2026-09-08 21:57:07 +0530

    Add DFS implementation in Graph class
    Implemented a depth-first search (DFS) algorithm in a Graph class to find an element.

---
+++ 06_DFS _Search_elem.py
@@ -0,0 +1,32 @@
+class Graph:
+    def __init__(self,vertex):
+        self.mat=[[0]*vertex for _ in range(vertex)]
+        self.size=vertex
+    def add_edge(self,src,dest):
+        if (0<= src < self.size and 0 <= dest < self.size):
+            self.mat[src][dest]=1
+        else:
+            print("Invalid ")
+    def dfs(self,src,target):
+        visited=[False]*self.size
+        stack=[src]
+        while(stack):
+            v=stack.pop()
+            if (visited[v]==False):
+                print(v,end=" ")
+                visited[v]=True
+                if v==target:
+                    print("Elemnt found ")
+                    return
+
+            for i in range(self.size):
+                if self.mat[v][i]==1 and visited[i]==False:
+                    stack.append(i)
+    
+G=Graph(6)
+G.add_edge(0,1)
+G.add_edge(0,2)
+G.add_edge(2,4)
+G.add_edge(2,3)
+G.add_edge(3,5)
+G.dfs(0,3)
